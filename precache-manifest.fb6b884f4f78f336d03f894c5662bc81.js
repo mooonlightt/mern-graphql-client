9dcc0237a6e3eab4e4456b8cfb3daf2078a680c9
mooonlightt <mooonlightt44@gmail.com>
2021-03-26 15:22:38 -0300

    Updates

---
+++ precache-manifest.fb6b884f4f78f336d03f894c5662bc81.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9154f8d8a4901962a8cc6fc05fb79f27",
+    "url": "/mern-graphql-client/index.html"
+  },
+  {
+    "revision": "c48a67b49118d59b636e",
+    "url": "/mern-graphql-client/static/css/2.ce95ad81.chunk.css"
+  },
+  {
+    "revision": "5622d1353107c9ae3ca3",
+    "url": "/mern-graphql-client/static/css/main.ee287267.chunk.css"
+  },
+  {
+    "revision": "c48a67b49118d59b636e",
+    "url": "/mern-graphql-client/static/js/2.5804c667.chunk.js"
+  },
+  {
+    "revision": "5622d1353107c9ae3ca3",
+    "url": "/mern-graphql-client/static/js/main.111e49f4.chunk.js"
+  },
+  {
+    "revision": "fa48d50bef4f0c18c69d",
+    "url": "/mern-graphql-client/static/js/runtime~main.4c9eb065.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/mern-graphql-client/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/mern-graphql-client/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/mern-graphql-client/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/mern-graphql-client/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/mern-graphql-client/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/mern-graphql-client/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/mern-graphql-client/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/mern-graphql-client/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/mern-graphql-client/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/mern-graphql-client/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/mern-graphql-client/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/mern-graphql-client/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/mern-graphql-client/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/mern-graphql-client/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/mern-graphql-client/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/mern-graphql-client/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
