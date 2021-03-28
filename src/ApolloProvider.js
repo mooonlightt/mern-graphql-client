49edd7a41eb310d18d389547c9eb5fc3d9ddd375
mooonlightt <mooonlightt44@gmail.com>
2021-03-28 20:19:02 -0300

    update 05

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,9 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://merngraphql.herokuapp.com'
+
+  uri:'https://reactgraphqlapp.herokuapp.com/'
+  //uri: 'http://merngraphql.herokuapp.com'
   //uri: 'http://localhost:5000'
 });
 
